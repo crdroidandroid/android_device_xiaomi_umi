e90681651ed653fdeff236eaf7af21ba62cd049e
mikeNG <mkbestas@lineageos.org>
2021-02-21 00:33:37 +0800

    umi: Switch to standalone extract utils

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -7,13 +7,11 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+#!/bin/bash
 #
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# Copyright (C) 2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
 #
 
 set -e
@@ -21,29 +19,27 @@ set -e
 DEVICE=umi
 VENDOR=xiaomi
 
-INITIAL_COPYRIGHT_YEAR=2021
-
 # Load extract_utils and do some sanity checks
 MY_DIR="${BASH_SOURCE%/*}"
-if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
+if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
-ROOT="$MY_DIR"/../../..
+ANDROID_ROOT="${MY_DIR}/../../.."
 
 HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
-if [ ! -f "$HELPER" ]; then
-    echo "Unable to find helper script at $HELPER"
+if [ ! -f "${HELPER}" ]; then
+    echo "Unable to find helper script at ${HELPER}"
     exit 1
 fi
-. "$HELPER"
+source "${HELPER}"
 
-# Initialize the helper for device
-setup_vendor "$DEVICE" "$VENDOR" "$ROOT"
+# initialize the helper
+setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false
 
-# Copyright headers and guards
-write_headers "$DEVICE"
+# Warning headers and guards
+write_headers
 
 # The standard device blobs
-write_makefiles "$MY_DIR"/proprietary-files.txt true
+write_makefiles "${MY_DIR}/../${DEVICE}/proprietary-files.txt" true
 
-# We are done!
+# Finish
 write_footers
